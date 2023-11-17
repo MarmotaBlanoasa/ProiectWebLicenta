a492e2fdebee9286d6e806709adddf986cab519d
Catalin Negrea <catalin.florin.negrea@stud.ubbcluj.ro>
2023-11-17 13:00:20 +0200

    modificare

--- conectare.php
+++ conectare.php
@@ -3,7 +3,7 @@
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'proiectweb';
+$DATABASE_NAME = 'eveniment';
 
 // Încercați să vă conectați folosind informațiile de mai sus
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
--- inserare_sesiune.php
+++ inserare_sesiune.php
@@ -1,46 +1,58 @@
 <?php
     include("conectare.php");
     $error = '';
+
+    // Extrage lista de evenimente
+    $resultEveniment = $mysqli->query("SELECT ID_Eveniment, Nume_Eveniment FROM eveniment");
+    $evenimente = $resultEveniment->fetch_all(MYSQLI_ASSOC);
+
     if (isset($_POST['submit']))
     {
         $ID_Eveniment = htmlentities($_POST['ID_Eveniment'], ENT_QUOTES);
         $Nume_Sesiune = htmlentities($_POST['Nume_Sesiune'], ENT_QUOTES);
-        if ($ID_Eveniment == ''|| $Nume_Sesiune=='')
+
+        if ($ID_Eveniment == '' || $Nume_Sesiune == '')
         {
-            $error = 'ERROR:Campuri goale!';
+            $error = 'ERROR: Campuri goale!';
         }
         else
         {
-            if ($stmt = $mysqli->prepare("INSERT into sesiune ( ID_Eveniment, Nume_Sesiune ) VALUES ( ?, ?)"))
+            if ($stmt = $mysqli->prepare("INSERT INTO sesiune (ID_Eveniment, Nume_Sesiune) VALUES (?, ?)"))
             {
-                $stmt->bind_param("ss", $ID_Eveniment,$Nume_Sesiune);
+                $stmt->bind_param("is", $ID_Eveniment, $Nume_Sesiune);
                 $stmt->execute();
                 $stmt->close();
             }
             else 
             {
-                echo "ERROR:Nu se poate executa insert.";
+                echo "ERROR: Nu se poate executa insert.";
             }
         }
     }
     $mysqli->close();
 ?>
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
     <head> 
-        <title><?php echo "Inserare inregistrare"; ?> </title>
+        <title>Inserare Înregistrare</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     </head> 
     <body>
-        <h1><?php echo "Inserare inregistrare"; ?></h1>
+        <h1>Inserare Înregistrare</h1>
         <?php if ($error != '') 
-                {
-                echo "<div style='padding:4px; border:1px solid red; color:red'>" . $error."</div>";
-                } 
+            {
+                echo "<div style='padding:4px; border:1px solid red; color:red'>" . $error . "</div>";
+            } 
         ?>
         <form action="" method="post">
             <div>
-                <strong>ID Eveniment: </strong> <input type="text" name="ID_Eveniment" value=""/><br/>
+                <strong>Eveniment: </strong>
+                <select name="ID_Eveniment">
+                    <?php foreach ($evenimente as $eveniment) {
+                        echo "<option value='" . $eveniment['ID_Eveniment'] . "'>" . $eveniment['Nume_Eveniment'] . "</option>";
+                    } ?>
+                </select><br/>
                 <strong>Nume Sesiune: </strong> <input type="text" name="Nume_Sesiune" value=""/><br/>
                 <br/>
                 <input type="submit" name="submit" value="Submit" />
